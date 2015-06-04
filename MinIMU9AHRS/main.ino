d6df20fde508dc74f0280dd144b7c908fa5b0b5d
nvu99 <thaidaivu@gmail.com>
2015-06-04 21:44:05 +0200

    v1.3

--- MinIMU9AHRS/main.ino
+++ MinIMU9AHRS/main.ino
@@ -2,10 +2,13 @@
 #include <Servo.h>
 #include <math.h>
 
+long duration;
+long cm;
+
 const int mCount = 4;
 Servo motors[mCount];
 
-int ports [mCount] = {2, 3, 4, 5}; //Port of each servo
+int ports [mCount] = {4, 5, 6, 7}; //Port of each servo
 int motorPos [mCount];
 
 float volt_vPow = 4.7;
@@ -13,7 +16,7 @@ float volt_r1 = 100000;
 float volt_r2 = 10000;
 
 void setup() {
-   pololuSetup();
+  pololuSetup();
   for (int i = 0; i < mCount; ++i) {
     motors [i].attach (ports [i]);
     motorPos [i] = 0;
@@ -21,7 +24,7 @@ void setup() {
   }
   //Setup serial connection
   Serial.begin(115200);
-  Serial.setTimeout(10);
+  Serial.setTimeout(2);
   
   delay(3000);
   
@@ -36,37 +39,59 @@ void setup() {
 //Read commands and set the motors
 void loop() {
   
+  //noInterrupts();
   receiveData();
   receiveData();
   
+  //interrupts();
   sensors();
 }
 
 void sensors() {
  float volt_v = (analogRead(0) * volt_vPow) / 1024.0;
  float volt_v2 = volt_v / (volt_r2 / (volt_r1 + volt_r2));
- float volt_def = floor(volt_v2 * 100) / 100;
+ //float volt_def = floor(volt_v2 * 100) / 100;
  
   for (int i = 0; i < 4; ++i) {
   Serial.print(motors [i].read());
   receiveData();
   Serial.print(" "); 
   }
-
   
-  
+  distanceSensor();
   pololuLoop();
-  String s = "" + String (accel_x);
-  s += " " + String(accel_y);
-  s += " " + String(accel_z);
+  
+  delay(2);
+  
+  String s = "" + String (accel_x / 26.2 , 2);
+  s += " " + String(accel_y / 26.2 , 2);
+  s += " " + String(accel_z / 26.2 , 2);
   Serial.print (s); Serial.print(" ");
   Serial.print(ToDeg(roll)); Serial.print(" ");
   Serial.print(ToDeg(pitch)); Serial.print(" ");
   Serial.print(ToDeg(yaw)); Serial.print(" ");
-  Serial.print(pressure); Serial.print(" ");
+  /*Serial.print(pressure); Serial.print(" ");
   Serial.print(altitude); Serial.print(" ");
-  Serial.print(temperature); Serial.print(" ");
-  Serial.print(volt_def, DEC); Serial.println(" ");
+  Serial.print(temperature); Serial.print(" ");*/
+  Serial.print(volt_v2, 2); Serial.print(" ");
+  Serial.print(cm); Serial.println("");
+  
+  if (cm >= 150)
+  {
+   delay(2); 
+  }
+  else if (cm >= 50)
+  {
+   delay(10); 
+  }
+  else if (cm >= 1)
+  {
+   delay(17); 
+  }
+  else
+  {
+   delay(2); 
+  }
 }
 
 void receiveData() {
@@ -77,6 +102,23 @@ void receiveData() {
     }
   }       
   for (int i = 0; i < mCount; ++i) {
-     motors [i].write (motorPos [i]); 
+     motors [i].write (motorPos [i]);
   }
+}
+
+void distanceSensor()
+{
+   const int pingPin = 12;
+   int inPin = 11;
+  
+   pinMode(pingPin,OUTPUT);
+   
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(5);
+   digitalWrite(pingPin, LOW);
+   
+   pinMode(inPin,INPUT);
+   duration = pulseIn(inPin, HIGH, 16000);
+   
+   cm = microsecondsToCentimeters(duration);
 }
